ac3a12467ebe1d53314cb2d1dd2ecf2a02aab8f9
Bryce B <bryce.boddy@gmail.com>
2021-01-05 07:28:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  document.getElementById('text').innerHTML = "This is really cool!"
 });
-
-document.getElementById('text').innerHTML = "This is really cool!"
